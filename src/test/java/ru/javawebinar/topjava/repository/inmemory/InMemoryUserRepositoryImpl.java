0dbb01447a89cd4f1fdb259cd5796a2c0bccbd99
= <=>
2019-03-03 12:33:34 +0200

    3 07 add junit

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
@@ -8,11 +8,17 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.UserTestData.USER;
+
 @Repository
 public class InMemoryUserRepositoryImpl extends InMemoryBaseRepositoryImpl<User> implements UserRepository {
 
-    static final int USER_ID = 1;
-    static final int ADMIN_ID = 2;
+    public void init() {
+        entryMap.clear();
+        entryMap.put(UserTestData.USER_ID, USER);
+        entryMap.put(UserTestData.ADMIN_ID, ADMIN);
+    }
 
     @Override
     public List<User> getAll() {
